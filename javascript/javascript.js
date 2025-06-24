b321faef772c6eb484c08f178a4489bd71ffaf3b
aleria <kendarrelle.talisic@gmail.com>
2025-06-24 23:50:51 +0800

    made changes to about page and removed the container fixed the exes page

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,3 +2,19 @@ function toggleBackground() {
       document.body.classList.toggle("white-bg");
       document.body.classList.toggle("gradient-bg");
     }
+
+ const floatingNav = document.getElementById('floating-nav');
+    const floatingToggle = document.getElementById('floating-toggle');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            floatingNav.classList.add('show');
+        } else {
+            floatingNav.classList.remove('show');
+            floatingNav.classList.remove('menu-open'); // close menu when hidden
+        }
+    });
+
+    floatingToggle.addEventListener('click', () => {
+        floatingNav.classList.toggle('menu-open');
+    });
